accc50c08c3715df2e6f7bc1eccaedb035439688
grestonian <gsingh0719@gmail.com>
2019-11-04 10:35:51 +0530

    NOTHIN

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ function myFunction() {
   const main = document.querySelector('.main');
   const logo = document.querySelector('.logo');
   const line = document.querySelector('.line');
+  const motto = document.querySelector('.motto');
 
   main.classList.toggle('main-tog');
   logo.classList.toggle('logo-tog');
   line.classList.toggle('line-tog');
+  motto.classList.toggle('motto-tog');
 
 }
