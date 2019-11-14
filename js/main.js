0728f584cf378b358f0d420c6b49fc1131664c27
grestonian <gsingh0719@gmail.com>
2019-11-14 11:51:38 +0530

    transitions handled, chatbox begun

--- js/main.js
+++ js/main.js
@@ -13,14 +13,22 @@ function myFunction() {
   line.classList.toggle('line-tog');
   motto.classList.toggle('motto-tog');
   logo.classList.toggle('logo-tog');
-  chtbt.classList.toggle('chatbot-tog')
-  btn.classList.toggle('button-tog')
+  chtbt.classList.toggle('chatbox-togg')
+  
+
 
   if(btn.innerText === "Begin") {
     btn.innerHTML = "<span>Go Back</span>";
+    btn.style.marginLeft = '0%'
+    btn.style.marginTop = '15%'
+    btn.style.transform = 'scale3d(0.5, 0.8, 1)'
   }
-  else
+  else {
     btn.innerHTML = "<span>Begin</span>";
-  
+    btn.style.marginLeft = '40%'
+    btn.style.marginTop = '0%'
+    btn.style.transform = 'scale3d(1, 1, 1)'
+  }
+    
 
 }
